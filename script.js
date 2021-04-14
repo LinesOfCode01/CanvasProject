d29b48a70fd51c26610bfd51d68e175d8518281f
Joe <joeyballesteros.wfg@gmail.com>
2021-04-14 09:50:23 -0700

    updated start

--- script.js
+++ script.js
@@ -123,9 +123,3 @@ function startGame() {
     animate();
 
 };
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-let raod = new Image();
-road.src = "resources/background1.png";
