59431fd76909b9a9d4b0ff96ae3b75bd1a95ecef
Joe <joeyballesteros.wfg@gmail.com>
2021-04-14 12:20:05 -0700

    updated canvas

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-window.onload = () => {
-    document.getElementById('startButton').onclick = () => {
-        startGame();
-    }
-};
+// window.onload = () => {
+//     document.getElementById('startButton').onclick = () => {
+//         startGame();
+//     }
+//     document.getElementById('exitButton').onclick = () => {
+//         window.location.href = "start.html";
+//     }
+// };
 
-    const canvas = document.querySelector(`#canvas`);
-    const ctx = canvas.getContext(`2d`);
-    ctx.drawImage(roadImg, 1000, 600, 1000, 900);
 
-function startGame(){   
+// function startGame() {
+//Global variables
+const canvas = document.querySelector(`#canvas`);
+const ctx = canvas.getContext(`2d`);
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+// ctx.drawImage(roadImg, 1000, 600, 1000, 900);
 
-//ROAD INFO
+//ROAD INFOroa
 
-    let roadImg = new Image();
-    roadImg.src = './assets/overheadRoad2.png';
-    roadImg.onload = function(){
-    
-    }
+let roadImg = new Image();
+roadImg.src = './assets/overheadRoad2.png';
+roadImg.onload = function () {
+    ctx.drawImage(roadImg, canvas.width - 500, 0, 500, canvas.height)
+}
 
 //CAR INFO
 
-    let carImg = new Image();
-    carImg.src = './assets/PoliceTrans.png';
+let carImg = new Image();
+carImg.src = './assets/PoliceTrans.png';
 
-    let car = {
-        x: 220,
-        y: 550,
-        h: 100,
-        w: 60,
+let car = {
+    x: 900,
+    y: canvas.height - 100,
+    h: 100,
+    w: 60,
 
-        draw: function () {
-            ctx.drawImage(carImg, this.x, this.y, this.w, this.h)
-        }
+    draw: function () {
+        ctx.drawImage(carImg, this.x, this.y, this.w, this.h)
     }
+}
 
 //VILLIAN INFO
 
 //     class Villan {
 //         constructor(x, y, w, h, img){
-    
+
 //             this.health =200
 //             this.x = x;
 //             this.y = y;
@@ -48,9 +54,9 @@ function startGame(){
 //         }
 //         draw = () =>{
 //             if(this.health > 0){
-    
+
 //                 ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
-    
+
 //             }
 //         }
 
@@ -81,100 +87,100 @@ function startGame(){
 //   }
 
 //CAR CONTROLS
-    window.onkeydown = function (e) {
-        console.log(e.key)
+window.onkeydown = function (e) {
+    console.log(e.key)
 
-        if (e.key === 'ArrowLeft') {
-            if (car.x > 0) {
-                car.x -= 15
-            }
-        }
-
-        if (e.key === 'ArrowRight') {
-            if (car.x < 440) {
-                car.x += 15
-            }
-        }
-
-        if (e.key === 'ArrowUp') {
-            if (car.y > 0) {
-                car.y -= 15
-            }
-        }
-
-        if (e.key === 'ArrowDown') {
-            if (car.y < 440) {
-                car.y += 15
-            }
+    if (e.key === 'ArrowLeft') {
+        if (car.x > 0) {
+            car.x -= 15
         }
     }
+
+    if (e.key === 'ArrowRight') {
+        if (car.x < 440) {
+            car.x += 15
+        }
+    }
+
+    if (e.key === 'ArrowUp') {
+        if (car.y > 0) {
+            car.y -= 15
+        }
+    }
+
+    if (e.key === 'ArrowDown') {
+        if (car.y < 440) {
+            car.y += 15
+        }
+    }
+}
 
 //COLLISION DETECTION
 
-    function detectCollision(rect1, rect2) {
-        if (rect1.x < rect2.x + rect2.w && rect1.x + rect1.w > rect2.x && rect1.y < rect2.y + rect2.h && rect1.y + rect1.h > rect2.y) {
+function detectCollision(rect1, rect2) {
+    if (rect1.x < rect2.x + rect2.w && rect1.x + rect1.w > rect2.x && rect1.y < rect2.y + rect2.h && rect1.y + rect1.h > rect2.y) {
 
-            console.log("COLLISION")
-            cancelAnimationFrame(gameInt)
-            alert("Game Over")
-        }
+        console.log("COLLISION")
+        cancelAnimationFrame(gameInt)
+        alert("Game Over")
     }
+}
 
 //OBSTACLE CODE
 
-    // class Obstacle {
-    //     constructor(x, y, w, h) {
-    //         this.x = x;
-    //         this.y = y;
-    //         this.w = w;
-    //         this.h = h;
+// class Obstacle {
+//     constructor(x, y, w, h) {
+//         this.x = x;
+//         this.y = y;
+//         this.w = w;
+//         this.h = h;
 
-    //         this.color = `rgb(${Math.random() * 225}, ${Math.random() * 255}, ${Math.random() * 255}`
-    //     }
+//         this.color = `rgb(${Math.random() * 225}, ${Math.random() * 255}, ${Math.random() * 255}`
+//     }
 
-    //     draw = () => {
-    //         ctx.fillStyle = this.color
-    //         ctx.fillRect(this.x, this.y, this.w, this.h)
-    //     }
+//     draw = () => {
+//         ctx.fillStyle = this.color
+//         ctx.fillRect(this.x, this.y, this.w, this.h)
+//     }
 
-    //     move = () => {
-    //         this.y += 5;
-    //     }
-    // }
+//     move = () => {
+//         this.y += 5;
+//     }
+// }
 
-    // let obstacles = [] //Part of setInterval function below:
+// let obstacles = [] //Part of setInterval function below:
 
-    // setInterval (function () {
-    //     obstacles.push(new Obstacle(Math.random() * 450, 0, Math.random() * 200 + 50, 30))
+// setInterval (function () {
+//     obstacles.push(new Obstacle(Math.random() * 450, 0, Math.random() * 200 + 50, 30))
 
-    //     score += 1
-    // }, 1000)
+//     score += 1
+// }, 1000)
 
-    let gameInt = null;
+let gameInt = null;
 
-    let score = 0;
+let score = 0;
 
 
 //ANIMATE FUNCTION
-    function animate() {
-        gameInt = requestAnimationFrame(animate)
-        ctx.clearRect(0, 0, 500, 700)
+function animate() {
+    gameInt = requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, 500, 700)
 
-        ctx.drawImage(roadImg, 0, 0, 500, 700)
+    // ctx.drawImage(roadImg, 0, 0, 500, 700)
 
-        ctx.font = '30px Arial';
+    ctx.font = '30px Arial';
 
-        ctx.fillText(score, 10, 30, 50, 00)
+    ctx.fillText(score, 10, 30, 50, 00)
 
-        car.draw();
+    car.draw();
 
-        obstacles.forEach(eachObstacle => {
-            eachObstacle.move()
-            eachObstacle.draw()
-            detectCollision(car, eachObstacle)
-        })
-    }
+    obstacles.forEach(eachObstacle => {
+        eachObstacle.move()
+        eachObstacle.draw()
+        detectCollision(car, eachObstacle)
+    })
+}
 
-    animate();
+animate();
 
-};
+// };
