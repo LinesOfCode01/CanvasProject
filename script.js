9bc8deb95752ca55d5d18e9c844cb5e5699cef52
Joe <joeyballesteros.wfg@gmail.com>
2021-04-14 08:03:01 -0700

    canvas

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+let raod = new Image();
+road.src = "resources/background1.png";
+
+drawFireballs();
