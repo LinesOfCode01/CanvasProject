16aaa1267562ba1add783fd3bd1a8c4216336672
Joe <joeyballesteros.wfg@gmail.com>
2021-04-14 08:09:35 -0700

    updated script

--- script.js
+++ script.js
@@ -3,6 +3,4 @@ const ctx = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 let raod = new Image();
-road.src = "resources/background1.png";
-
-drawFireballs();
+road.src = "";
